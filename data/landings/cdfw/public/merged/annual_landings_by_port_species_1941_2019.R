89bd423716921bb0e9fab809d68dd939ab20adff
Chris Free <cfree@iMac-4.local>
2021-02-02 08:50:59 -0800

    work on species names and port data merge

--- data/landings/cdfw/public/merged/annual_landings_by_port_species_1941_2019.R
+++ data/landings/cdfw/public/merged/annual_landings_by_port_species_1941_2019.R
@@ -17,7 +17,8 @@ plotdir <- "data/landings/cdfw/public/merged/figures"
 data_fb_orig <- readRDS("data/landings/cdfw/public/fish_bulletins/processed/CDFW_1941_1976_landings_by_port_species.Rds")
 
 # Read website data
-data_web_orig <- readRDS("data/landings/cdfw/public/website/by_port/processed/CDFW_2000_2019_landings_by_port.Rds")
+# data_web_orig <- readRDS("data/landings/cdfw/public/website/by_port/processed/CDFW_2000_2019_landings_by_port.Rds")
+data_web_orig <- read.csv("data/landings/cdfw/public/website/by_port/processed/CDFW_2000_2019_landings_by_port_expanded.csv", as.is=T)
 
 
 # Build data
@@ -32,6 +33,8 @@ colnames(data_web_orig)
 
 # Format web data
 data_web <- data_web_orig %>% 
+  # Remove some columns
+  select(-c(sci_name, level, taxa_group1, taxa_group2, environment)) %>% 
   # Rename
   rename(port_complex=area) %>% 
   # Add source/table
@@ -51,10 +54,15 @@ data_web <- data_web_orig %>%
   mutate(type="Landings") %>% 
   # Add port name
   mutate(port_orig=port) %>% 
+  # Fix presentation
+  mutate(presentation=recode(presentation, "whole"="not specified")) %>% 
   # Arrange
   select(source, table, port_complex, port_orig, port, type, year, 
-         comm_name_orig, value_usd, landings_lb, landings_kg, everything()) %>% 
+         comm_name_orig, comm_name, presentation, value_usd, landings_lb, landings_kg, everything()) %>% 
   select(-filename)
+
+# Check common names
+wcfish::check_names(data_web$comm_name)
 
 # Format Fish Bulletin data
 data_fb <- data_fb_orig %>%
@@ -62,6 +70,8 @@ data_fb <- data_fb_orig %>%
   rename(comm_name_orig=species) %>% 
   # Add pounds in kg
   mutate(landings_kg=measurements::conv_unit(landings_lb, "lbs", "kg")) %>% 
+  # Add presentation
+  mutate(presentation="not specified") %>% 
   # Arrange
   select(source, table, port_complex, port_orig, port, type, year, 
          comm_name_orig, value_usd, landings_lb, landings_kg, everything())
@@ -78,11 +88,15 @@ freeR::complete(data) # a few missing values in web, many missing landings in FB
 range(data$year)
 table(data$port_complex)
 table(data$port)
-table(data$comm_name_orig)
+table(data$comm_name)
+table(data$presentation)
 
+sdata <- data %>% 
+  filter(comm_name=="Crayfish" | comm_name_orig=="Crayfish")
 
 # Plot data
 ################################################################################
+
 
 
 
