da53ed4b28e3786ed4817e80ca681e1d8ab6a558
Chris Free <cfree@iMac-4.local>
2021-02-02 18:24:08 -0800

    new code to merge nvessels

--- data/landings/cdfw/public/merged/number_of_commercial_vessels.R
+++ data/landings/cdfw/public/merged/number_of_commercial_vessels.R
@@ -14,12 +14,10 @@ outdir <- "data/landings/cdfw/public/merged/data"
 plotdir <- "data/landings/cdfw/public/merged/figures"
 
 # Read FB 181 data (1934-1976)
-data_fb1_orig <- ""
+data_fb1_orig <- readRDS("data/landings/cdfw/public/fish_bulletins/processed/CDFW_1934_1976_nvessels_by_length.Rds")
 
 # Read FB 181 data (1976-1999)
 data_fb2_orig <- read.csv("data/landings/cdfw/public/fish_bulletins/raw/fb181/processed/FB181_Table3_licenced_fishermen_vessels.csv")
-
-# Read website data
 
 
 # Build data
