79fb2311920c7724a27c7c29226bfff6e1c0b4f5
camilavargasp <cvargas@bren.ucsb.edu>
2021-02-12 16:33:16 -0800

    progress on finalizing CPFV

--- data/landings/cdfw/public/fish_bulletins/1936_1986_nfish_party_vessels_by_species.R
+++ data/landings/cdfw/public/fish_bulletins/1936_1986_nfish_party_vessels_by_species.R
@@ -9,6 +9,8 @@ rm(list = ls())
 
 # Packages
 library(tidyverse)
+library(freeR)
+library(wcfish)
 
 # Directories
 outdir <- "data/landings/cdfw/public/fish_bulletins/processed"
@@ -19,8 +21,10 @@ indir <- "data/landings/cdfw/public/fish_bulletins/raw/"
 ##Information to import all necesary tables
 fbs <- c(95, 102, 105, 108, 111, 117, 121, 125, 129, 161, 163, 166, 168, 170, 173)
 
+## not using data of any of these becasie FB121 has historical data with more details in species
+fb_exclude <- c("FB95", "FB102", "FB105", "FB108", "FB111", "FB117")
 
-table_name <- c("Table28", "Table47", "Table50", "Table40", "Table22", "Table22", "Table22", "Table22", "Table23", "Table22", "Table22", "Table22", "Table22", "Table22", "Table14")
+table_name <- c("Table28", "Table47", "Table50", "Table40", "Table22", "Table22","Table22", "Table22", "Table23", "Table22", "Table22", "Table22", "Table22", "Table22", "Table14")
 
 
 fb_table_key <- tibble(fbs, table_name) %>% 
@@ -61,13 +65,15 @@ data_orig <- purrr::map_df(fb_table_key$path, function(x){
 ## Check totals
 
 totals <- data_orig %>% 
-  filter(str_detect(species, "fish"),
+  filter(!source %in% fb_exclude,
+         str_detect(species, "fish"),
          str_detect(species, "Total")) %>% 
   mutate(nfish_catch = as.numeric(nfish_catch)) %>% 
   rename(nfish_total = nfish_catch)
 
 totals_sum <- data_orig %>% 
-  filter(!str_detect(species, "Total"),
+  filter(!source %in% fb_exclude,
+         !str_detect(species, "Total"),
          !str_detect(species, "angler")) %>% 
   mutate(nfish_catch = as.numeric(nfish_catch)) %>% 
   group_by(year, source) %>% 
@@ -75,9 +81,7 @@ totals_sum <- data_orig %>%
   left_join(totals, by = c("year", "source")) %>% 
   mutate(dif = total_sum - nfish_total)
 
-##Check years that totals do not match!!!!!!!
-
-## There are 3 year that totals don't match: 
+## There are 3 year that totals don't match (all from FB173 Table14)
 ## 1977 difference = -360
 ## 1979 difference = -9
 ## 1980 difference = 10000
@@ -86,7 +90,8 @@ totals_sum <- data_orig %>%
 
 ## Note FB95 Table28, Rock bass included two species of kelp bass Paralabrax clathratus and sand bass P.nebulifer
 catch_data <- data_orig %>% 
-  filter(!str_detect(species, "angler"),
+  filter(!source %in% fb_exclude,
+         !str_detect(species, "angler"),
          !str_detect(species, "Total")) %>% 
   mutate(nfish_catch = as.numeric(nfish_catch)) %>% 
   # Format species
@@ -102,7 +107,10 @@ catch_data <- data_orig %>%
          comm_name_reg = recode(comm_name_reg,
                                 "Baas kdp and sand" = "Kelp/sand bass",
                                 'Bass kelp and sand' = "Kelp/sand bass",
+                                'Bass kelp'= "Kelp/sand bass",
                                 "Cod lins" = "Lingcod",
+                                'Califonia halibut'='California halibut',
+                                'Linccod'='Lingcod',
                                 "Gdfotua barracuda" = "California barracuda",
                                 "Ndfie mackerel" = "Pacific mackerel",
                                 "Other all" = "All other species",
@@ -124,14 +132,17 @@ catch_data <- data_orig %>%
                                 'Ocean whitefah'='Ocean whitefish',
                                 'Pacific bofuto'='Pacific bonito', 
                                 'Pacific booito'='Pacific bonito',
+                                'Pacific bonita'='Pacific bonito',
                                 'Rock fish'='Rockfish',
                                 'White seabaas'='White seabass', 
-                                'White seabasa'='White seabass'),
+                                'White seabasa'='White seabass',
+                                'Sea bass white'= 'White seabass',
+                                'Tail yellow'= "Yellowtail"),
          comm_name= wcfish::harmonize_names(comm_name_reg, from="comm", to="comm"),
          comm_name = case_when(is.na(comm_name) ~ comm_name_reg,
                                comm_name_reg == "Rock bass" ~ "Kelp/sand bass",
                                T ~ comm_name)) %>% 
-  select(source, table_name, year, comm_name, comm_name_reg, nfish_catch, region, region_type) %>% 
+  select(source, table_name, year, comm_name, comm_name_reg, nfish_catch, region, region_type) %>%
   ## Gets rid of duplicates. When values are provided in two FBs then it keeps just one of them
   distinct(comm_name, year, nfish_catch, region, region_type, .keep_all = T) %>% 
   ##fitering out duplicates that were not removed above
@@ -159,11 +170,19 @@ table(double_check_totals$year)
 #####################################################################################
 ## Angler data
 angler_data <- data_orig %>% 
-  filter(str_detect(species, "angler")) %>% 
+  filter(!source %in% fb_exclude,
+         str_detect(species, "angler")) %>% 
   mutate(species = str_remove(species, "Total") %>% str_trim(.) %>% str_to_lower(.),
-         nfish_catch = str_remove(nfish_catch, "\\-") %>% as.numeric(.)) %>%
+         nfish_catch = str_remove(nfish_catch, "\\-") %>% as.numeric(.),
+         effort_unit = word(species, -1),
+         effort_hours_calc = nfish_catch*24,
+         effort_hours = case_when(effort_unit == "days" ~ effort_hours_calc,
+                                  effort_unit == "anglers" ~ NA_real_,
+                                  T ~ nfish_catch)) %>%
   distinct(species, year, nfish_catch, .keep_all = T) %>% 
-  select(source, table_name, year, data_type = species, value = nfish_catch, region, region_type)
+  ##fitering out duplicates that were not removed above
+  filter(!(source == "FB125" & year == 1961)) %>% 
+  select(source, table_name, year, effort_org = nfish_catch, effort_unit, effort_hours, region, region_type)
 
 ##Note:
 ##1936-1959 - N of days
@@ -173,6 +192,16 @@ angler_data <- data_orig %>%
 ##1977-1986 - N of angles
 
 table(angler_data$year)
+
+##anglers data wide to match with catch data
+angler_data_wide <- angler_data %>% 
+  pivot_wider(names_from = effort_unit,
+              names_prefix = "n_",
+              values_from = effort_org) %>% 
+  filter(!(is.na(effort_hours) & is.na(n_anglers))) %>% 
+  relocate(effort_hours, .after = last_col())
+
+
 
 #####################################################################################
 ##Plot
