40721496999a5f9c046229bbe4c8aecb70372375
Chris Free <cfree14@gmail.com>
2022-01-25 09:08:49 -0800

    formatted PACFIN month data

--- data/landings/pacfin/format_pacfin_ALL006.R
+++ data/landings/pacfin/format_pacfin_ALL006.R
@@ -13,12 +13,6 @@ library(tidyverse)
 datadir <- "data/landings/pacfin/raw/ALL/ALL006"
 outputdir <- "data/landings/pacfin/processed"
 
-# Read data
-# Manually converted the XLS to an XLSX
-ca1_orig <- readxl::read_excel(file.path(datadir, "ALL006-California-1980---2021-All-Views.xlsx"), 
-                               sheet=2, skip=2, na=c("*", "-")) # landed units
-# ca2_orig <- readxl::read_excel(file.path(datadir, "ALL006-California-1980---2021-All-Views.xlsx"), sheet=4) # all units
-
 # Read codes
 port_key <- read.csv(file.path(outputdir, "pacfin_port_codes_clean.csv"), as.is=T)
 spp_key <- read.csv(file.path(outputdir, "pacfin_species_codes_clean.csv"), as.is=T)
@@ -30,14 +24,82 @@ spp_key <- read.csv(file.path(outputdir, "pacfin_species_codes_clean.csv"), as.i
 # Format data
 ################################################################################
 
+# Files to merges
+files2merge <- list.files(datadir)
+
+
+# Loop through files and merge
+x <- files2merge[1]
+data_orig <- purrr::map_df(files2merge, function(x){
+  
+  # Read data
+  fdata_orig <- read.csv(file.path(datadir, x), as.is=T)
+  
+  # Format data
+  fdata <- fdata_orig %>% 
+    janitor::clean_names("snake")
+  
+})
+
 # Format data
-ca1 <- ca1_orig %>% 
-  # Gather
-  gather(key="metric", value="value", 7:ncol(.)) %>% 
+data <- data_orig %>% 
   # Rename
-  janitor::clean_names("snake") %>% 
-  # Remove totals
-  filter(!is.na(species_code)) %>% 
-  # Convert value to numeric
-  mutate(value=as.numeric(value))
+  rename(year=landing_year, state=agency_code, mgmt_group_code=management_group_code, 
+         comm_name=pacfin_species_common_name, spp_code=pacfin_species_code) %>% 
+  # Format state
+  mutate(state=recode(state, "AT-SEA"="At-Sea", "C"="California", "O"="Oregon", "W"="Washington")) %>% 
+  # Gather columns
+  gather(key="metric_long", value="value", 7:ncol(.)) %>% 
+  # Extract month
+  mutate(month=substr(metric_long, 1, 3)) %>% 
+  # Spread by metric
+  filter(!grepl("total", metric_long)) %>% 
+  mutate(metric_long=substr(metric_long, 5, nchar(metric_long))) %>% 
+  spread(key="metric_long", value="value") %>% 
+  # Rename metrics
+  rename(value_usd=exvessel_revenue,
+         landings_mt=landed_weight_mtons,
+         price_usd_lb=landed_weight_ppp, 
+         confidential=confidential_flag) %>% 
+  # Convert to numeric
+  mutate(landings_mt=as.numeric(landings_mt),
+         price_usd_lb=as.numeric(price_usd_lb),
+         value_usd=as.numeric(value_usd)) %>% 
+  # Add landings weights
+  mutate(landings_kg=landings_mt*1000,
+         landings_lb=measurements::conv_unit(landings_kg, "kg", "lbs")) %>% 
+  # Derive price per pound (ppp)
+  # Interesting, these don't perfectly match reported values
+  mutate(price_usd_lb1=value_usd/landings_lb) %>% 
+  select(-price_usd_lb1) %>% 
+  # Reduce 
+  filter(!is.na(landings_mt) | !is.na(price_usd_lb) | !is.na(value_usd)) %>% 
+  # Add date
+  mutate(month=stringr::str_to_title(month),
+         date=lubridate::mdy(paste(month, "1,", year))) %>% 
+  # Format common name
+  mutate(spp_type=ifelse(grepl("NOM.", comm_name), "nominal", "actual"),
+         comm_name_parent=gsub("NOM. ", "", comm_name),
+         comm_name_parent=stringr::str_to_sentence(comm_name_parent),
+         comm_name=stringr::str_to_sentence(comm_name)) %>% 
+  # Format management group code
+  mutate(mgmt_group_code=recode(mgmt_group_code, 
+                                "XXXX SUBTOTAL"="Unknown")) %>% 
+  # Arrange
+  select(state, mgmt_group_code, complex, comm_name_parent, comm_name, spp_code, spp_type, year, month, date, 
+         landings_mt, landings_kg, landings_lb, price_usd_lb, value_usd, confidential, everything()) %>% 
+  arrange(state, mgmt_group_code, complex, comm_name_parent, comm_name, spp_code, year, date)
+
+# Inspect
+str(data)
+freeR::complete(data)
+table(data$year)
+table(data$month)
+table(data$mgmt_group_code)
+
+# Species
+spp_key <- count(data, comm_name_parent, comm_name, spp_code) %>% arrange(desc(n))
+
+# Export data
+saveRDS(data, file.path(outputdir, "PACFIN_ALL006_1980_2022_all_species_landings_by_month.Rds"))
 
